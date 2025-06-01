965bac43b7c99c67a0ab708583735b43841b4a04
AleksandrFedorov035 <aleksandrnosov2008@gmail.com>
2025-06-01 12:23:06 +0300

    add Trello

--- src/js/components/Trello.js
+++ src/js/components/Trello.js
@@ -2,9 +2,15 @@ export default class Trello {
     constructor(container) {
         this.container = container
 
+        this.inputFieldHandler = null;
+        this.buttonClickHandler = null;
+
+        this.actualElement;
+
         this.showForm = this.showForm.bind(this);
         this.container.addEventListener('click', e => {
             if (e.target.classList.contains('add-card')) {
+                e.target.style.display = "none"
                 this.showForm(e);
             }
         });
@@ -14,7 +20,7 @@ export default class Trello {
         return `
             <div class="trello">
             ${['TODO', 'In Progress', 'DONE'].map(status => {
-                return `
+            return `
                 <div class="item">
                     <div class="top-content">
                         <h1>${status}</h1>
@@ -32,14 +38,11 @@ export default class Trello {
                                 <div class="popup-close">
                                     <div class="popup-close-element"></div>
                                 </div>
-                                <div class="menu popup-menu">
-                                    <div class="menu-item"></div>
-                                </div>
                             </div>
                         </div>
                     </div>
                 </div>`
-            }).join('')}
+        }).join('')}
         `
     }
 
@@ -59,19 +62,85 @@ export default class Trello {
     }
 
     showForm(e) {
-        const bottomContent = e.currentTarget
-        const item = bottomContent.closest(".item")
-        console.log(item);
+        const item = e.target.closest(".item")
+        const popup = item.querySelector(".popup")
+        popup.style.display = "flex"
 
-        e.target.style.display = "none"
-        bottomContent.querySelector(".popup").style.display = "flex"
+        if (this.inputFieldHandler) popup.querySelector(".input-addcard").removeEventListener('keyup', this.inputFieldHandler);
+        if (this.buttonClickHandler) popup.querySelector("button").removeEventListener('click', this.buttonClickHandler);
 
-        bottomContent.querySelector(".input-addcard").addEventListener("change", () => this.addCardToHTML(item))
-        bottomContent.querySelector("button").addEventListener("click", () => this.addCardToHTML(item))
+        this.inputFieldHandler = e => {
+            if (e.code === "Enter") {
+                this.addCardToHTML(item);
+            }
+        }
+        this.buttonClickHandler = () => this.addCardToHTML(item);
+
+        popup.querySelector(".popup-close").addEventListener("click", () => this.closeForm(item))
+        popup.querySelector("button").addEventListener('click', this.buttonClickHandler)
+        popup.querySelector(".input-addcard").addEventListener('keyup', this.inputFieldHandler)
+    }
+
+    closeForm(item) {
+        item.querySelector(".popup").style.display = "none"
+        item.querySelector(".add-card").style.display = "block"
+        item.querySelector(".input-addcard").value = ""
     }
 
     addCardToHTML(item) {
         const value = item.querySelector(".input-addcard").value.trim()
-        console.log(value);
+        if (!value) return
+
+        const mainContent = item.querySelector(".main-content")
+
+        const existingCards = Array.from(mainContent.children);
+        const duplicateFound = existingCards.some(card => card.querySelector('p').innerText === value);
+        const error = item.querySelector(".errorMessage")
+        if (duplicateFound) {
+            if (error) return
+            const errorMessage = '<p style="color: red;" class="errorMessage">Карточка с таким текстом уже существует!</p>';
+            item.querySelector(".popup").insertAdjacentHTML("afterbegin", errorMessage);
+            return
+        }
+        if (error) error.remove()
+
+
+        const newElement = this.renderCard(value)
+        mainContent.insertAdjacentHTML("beforeend", newElement)
+
+        const lastAddedCard = mainContent.lastElementChild;
+        lastAddedCard.querySelector('.close').addEventListener('click', () => this.removeCard(lastAddedCard))
+        lastAddedCard.addEventListener("mousedown", e => {
+            if (e.target != lastAddedCard.querySelector('.close')) this.onMouseDown(e)
+        })
+
     }
+
+    removeCard(element) {
+        element.remove()
+    }
+
+    onMouseDown(e) {
+        this.actualElement = e.target;
+
+        this.actualElement.classList.add("dragged")
+
+        document.documentElement.addEventListener('mouseup', e => this.onMouseUp(e));
+        document.documentElement.addEventListener('mouseover', e => this.onMouseOver(e));
+    }
+
+    onMouseUp(e) {
+        const mouseUpItem = e.target;
+
+        this.actualElement.classList.remove('dragged');
+
+        document.documentElement.removeEventListener('mouseup', e => this.onMouseUp(e));
+        document.documentElement.removeEventListener('mouseover', e => this.onMouseOver(e));
+    }
+
+    onMouseOver(e) {
+        this.actualElement.style.top = e.clientY + 'px';
+        this.actualElement.style.left = e.clientX + 'px';
+    }
+
 }
